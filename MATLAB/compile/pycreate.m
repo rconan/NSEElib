04ef9b6f6cedc7e715718aa5039af9aaab01eca6
Felipe <feippolito@gmail.com>
2019-09-10 16:57:37 -0300

    pycreate documentation

--- MATLAB/compile/pycreate.m
+++ MATLAB/compile/pycreate.m
@@ -1,15 +1,18 @@
 function pycreate(blockname, Ts, flag_class, pyversion, pypath)
 %{
-                     _ _     ___      _    ___         _             
-   __ ___ _ __  _ __(_) |___/ __|_  _| |__/ __|_  _ __| |_ ___ _ __  
-  / _/ _ \ '  \| '_ \ | / -_)__ \ || | '_ \__ \ || (_-<  _/ -_) '  \ 
-  \__\___/_|_|_| .__/_|_\___|___/\_,_|_.__/___/\_, /__/\__\___|_|_|_|
-               |_|                             |__/                  
-  
-                                                        Version 0.0.1
+                               _____                    _        
+                              / ____|                  | |       
+                _ __   _   _ | |      _ __  ___   __ _ | |_  ___ 
+               | '_ \ | | | || |     | '__|/ _ \ / _` || __|/ _ \
+               | |_) || |_| || |____ | |  |  __/| (_| || |_|  __/
+               | .__/  \__, | \_____||_|   \___| \__,_| \__|\___|
+               | |      __/ |                                    
+               |_|     |___/                                     
+
+                                                        Version 0.0.2
   
                         Marcelo Lafeta <marcelolafeta.nsee@gmail.com>
-                               Felipe Ippolito <feippolito@gmail.com>
+                           Felipe Ippolito <felipeippolito@gmail.com>
 
   ===================================================================
   
@@ -25,7 +28,7 @@ function pycreate(blockname, Ts, flag_class, pyversion, pypath)
                    individual functions. The structure of both are
                    presented below.
 
-              - pyversion (num): the python version on your machine,
+              - pyversion (str): the python version on your machine,
                    default is 3.7.
 
               - pypath (str): the path to your python environment on the
@@ -38,10 +41,10 @@ function pycreate(blockname, Ts, flag_class, pyversion, pypath)
                     created by the MATLAB compiler. This folder is just a
                     backup for your project.
 
-            - ${blockname}System: here it will have the same files as in
-                    the first folder, with two new added: the Makefile and 
-                    the ${blockname}.pyx. This is the folder that you will
-                    make you library with !make on terminal.
+            - ${blockname}Controller: here it will have the same files as 
+                    in the first folder, with two new added: the Makefile 
+                    and the ${blockname}.pyx. This is the folder that you 
+                    will make you library with !make on terminal.
 
   =================================================================== 
 
@@ -50,29 +53,6 @@ function pycreate(blockname, Ts, flag_class, pyversion, pypath)
         technique of making the directory with !make and the structure
         provided by gcc GNU compiler.
 
-  =================================================================== 
-  
-  Copyright (C) [2019] Marcelo Lafeta and Felipe Ippolito
-  
- *  Permission is hereby granted, free of charge, to any person obtaining
- *  a copy of this software and associated documentation files (the
- *  "Software"), to deal in the Software without restriction, including
- *  without limitation the rights to use, copy, modify, merge, publish,
- *  distribute, sublicense, and/or sell copies of the Software, and to
- *  permit persons to whom the Software is furnished to do so, subject to
- *  the following conditions:
- *  
- *  The above copyright notice and this permission notice shall be
- *  included in all copies or substantial portions of the Software.
- *  
- *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- *  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- *  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- *  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
- *  CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
- *  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
- *  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-  
   ===================================================================
   
  %}
@@ -99,7 +79,8 @@ set_param(model_name, 'SolverType', 'Fixed-step',...
 save_system(filename);
 %% 
 
-simulink_block = string(model_name) + "/" + string(blockname); % gcb(model_name); % Get Simulink/Block
+% gcb(model_name); % Get Simulink/Block
+simulink_block = string(model_name) + "/" + string(blockname); 
 
 split_string = strsplit(simulink_block,'/'); % Separate string
 block_name = string(split_string(2)); % Block name
@@ -131,7 +112,8 @@ end
 controller_path = cd;
 h_path = controller_path + "/" + block_name + ".h";
 
-[reference_size, feedback_size, output_size] = utils.GetSizes(h_path, 'Reference','Feedback','Output');
+[reference_size, feedback_size, output_size] = utils.GetSizes(h_path...
+    , 'Reference','Feedback','Output');
 
 %% Check for _data.o file
 
